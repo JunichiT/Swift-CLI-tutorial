ce9ffc8ab48071d69d46aeddb19a4aa676e12db1
Junichi Tsurukawa <junichi-tsurukawa@cookpad.com>
2017-06-10 19:28:03 +0900

    Add improved message output

--- Panagram/Panagram/ConsoleIO.swift
+++ Panagram/Panagram/ConsoleIO.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+enum OutputType {
+    case error
+    case standard
+}
+
 enum OptionType: String {
     case palindrome = "p"
     case anagram = "a"
@@ -41,5 +46,14 @@ class ConsoleIO {
 
     static func getOption(_ option: String) -> (option: OptionType, value: String) {
         return (OptionType(value: option), option)
+    }
+
+    static func writeMessage(_ message: String, to: OutputType = .standard) {
+        switch to {
+        case .standard:
+            print("\u{001B}[;m\(message)")
+        case .error:
+            fputs("\u{001B}[0;31m\(message)\n", stderr)
+        }
     }
 }
--- Panagram/Panagram/Panagram.swift
+++ Panagram/Panagram/Panagram.swift
@@ -11,44 +11,44 @@ class Panagram {
         let argCount = CommandLine.argc
         let argument = CommandLine.arguments[1]
         let (option, value) = ConsoleIO.getOption(argument.substring(from: argument.characters.index(argument.startIndex, offsetBy: 1)))
-        print("Argument count: \(argCount), Option: \(option), value: \(value)")
+        ConsoleIO.writeMessage("Argument count: \(argCount), Option: \(option), value: \(value)")
 
         switch option {
         case .anagram:
             guard argCount == 4 else {
                 if argCount > 4 {
-                    print("Too many argumants for option \(option.rawValue)")
+                    ConsoleIO.writeMessage("Too many argumants for option \(option.rawValue)", to: .error)
                 } else {
-                    print("Too few argumants for option \(option.rawValue)")
+                    ConsoleIO.writeMessage("Too few argumants for option \(option.rawValue)", to: .error)
                 }
                 return
             }
             let first = CommandLine.arguments[2]
             let second = CommandLine.arguments[3]
             if first.isAnagram(of: second) {
-                print("\(first) is an anagral of \(second)")
+                ConsoleIO.writeMessage("\(first) is an anagral of \(second)")
             } else {
-                print("\(first) is NOT an anagral of \(second)")
+                ConsoleIO.writeMessage("\(first) is NOT an anagral of \(second)")
             }
 
         case .palindrome:
             guard argCount == 3 else {
                 if argCount > 3 {
-                    print("Too many argumants for option \(option.rawValue)")
+                    ConsoleIO.writeMessage("Too many argumants for option \(option.rawValue)", to: .error)
                 } else {
-                    print("Too few argumants for option \(option.rawValue)")
+                    ConsoleIO.writeMessage("Too few argumants for option \(option.rawValue)", to: .error)
                 }
                 return
             }
             let string = CommandLine.arguments[2]
             let isPalindrome = string.isPalindrome()
-            print("\(string) is \(isPalindrome ? "" : "NOT ")a palindrome")
+            ConsoleIO.writeMessage("\(string) is \(isPalindrome ? "" : "NOT ")a palindrome")
 
         case .help:
             ConsoleIO.showUsage()
 
         case .unknown:
-            print("Unknown option")
+            ConsoleIO.writeMessage("Unknown option", to: .error)
             ConsoleIO.showUsage()
         }
     }
