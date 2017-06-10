597d6906c315bfbdb141a06b7b628e8e258dac1a
Junichi Tsurukawa <junichi-tsurukawa@cookpad.com>
2017-06-10 17:11:19 +0900

    Simply prints a usage information

--- Panagram/Panagram/Panagram.swift
+++ Panagram/Panagram/Panagram.swift
@@ -1,12 +1,13 @@
 //
-//  main.swift
+//  Panagram.swift
 //  Panagram
 //
 //  Created by junichi-tsurukawa on 2017/06/10.
 //  Copyright © 2017年 junichit. All rights reserved.
 //
 
-import Foundation
-
-let panagram = Panagram()
-panagram.staticMode()
+class Panagram {
+    func staticMode() {
+        ConsoleIO.showUsage()
+    }
+}
